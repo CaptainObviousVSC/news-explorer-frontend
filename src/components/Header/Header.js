9537f6245d060bce9764cda3d0aba792676c205d
Aziik <arslanfazilov93@gmail.com>
2021-03-11 22:04:39 +0300

    Исправлены ошибки

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,24 @@
 import React from 'react';
-import { Route, Link, Switch } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import './Header.css'
-function Header({onLoginPopup }) {
+function Header({ onLoginPopup }) {
     return (
         <header className="header">
-              <p className="header__logo">NewsExplorer</p>
-              <div className="header__text-box">
-            <Switch>
-                <Route path="/">
+            <p className="header__logo">NewsExplorer</p>
+            <input id="header__toggle" type="checkbox" className="header__toggle" />
+                <label class="header__button-for-mobile" for="header__toggle">
+                    <span></span>
+                </label>
+                <ul class="header__box-for-mobile">
+               <li><p className="header__main-link-selected_for-mobile">Главная</p></li>
+                <li><Link to="/savedNews" className="header__saved-news-link_for-mobile">Сохраненные статьи</Link></li>
+                <li><button className="header__authorize_for-mobile" onClick={onLoginPopup}>Авторизоваться</button></li>
+                </ul>
+            <div className="header__text-box">
                 <p className="header__main-link-selected">Главная</p>
-                    <Link to="/savedNews" className="header__saved-news-link">Сохраненные статьи</Link>
-                    {/* <button className="header__authorize" onClick={onLoginPopup}>Авторизоваться</button> */}
-                    {/* раскомментировать чтобы увидеть попапы                 */}
-                    </Route>
-            </Switch>
+                <Link to="/savedNews" className="header__saved-news-link">Сохраненные статьи</Link>
+                <button className="header__authorize" onClick={onLoginPopup}>Авторизоваться</button>
+                {/* раскомментировать чтобы увидеть попапы                 */}
             </div>
         </header>
     );
